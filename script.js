0d6e2720188822785dc4b83641e7b1a24b8d3761
Saksham Srivastava <ss.sakshamsrivastava02@gmail.com>
2024-07-12 22:51:49 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.querySelector(".sidebar").style.width = "250px";
+}
+
+// Close the sidebar
+function closeNav() {
+    document.querySelector(".sidebar").style.width = "0";
+}
